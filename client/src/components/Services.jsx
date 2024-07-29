67a34b930ffa24692d6bd5425d739419214087d5
Karan Anand <anandkarancompsci@gmail.com>
2024-07-28 22:00:37 -0700

    Services Section improved with modification

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -22,34 +22,34 @@ const Services = () => (
     <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
       <div className="flex-1 flex flex-col justify-start items-start">
         <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
-          Services that we
+          Services we
           <br />
-          continue to improve
+          constantly enhance
         </h1>
         <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-          The best choice for buying and selling your crypto assets, with the
-          various super friendly services we offer
+          The top choice for buying and selling your crypto assets, with our
+          wide range of user-friendly services
         </p>
       </div>
 
       <div className="flex-1 flex flex-col justify-start items-center">
         <ServiceCard
           color="bg-[#2952E3]"
-          title="Security gurantee"
+          title="Security guarantee"
           icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          subtitle="Your security is assured. We always protect privacy and uphold the quality of our products"
         />
         <ServiceCard
           color="bg-[#8945F8]"
           title="Best exchange rates"
           icon={<BiSearchAlt fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          subtitle="The best rates available. We always protect privacy and uphold the quality of our products"
         />
         <ServiceCard
           color="bg-[#F84550]"
           title="Fastest transactions"
           icon={<RiHeart2Fill fontSize={21} className="text-white" />}
-          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+          subtitle="Lightning-fast transactions. We always protect privacy and uphold the quality of our products"
         />
       </div>
     </div>
