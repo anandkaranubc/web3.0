250e7afda3d641ccfe2730b7e2770875e9c51e1d
Karan Anand <anandkarancompsci@gmail.com>
2024-07-28 21:21:25 -0700

    local progress saved still errors in sending ethereum

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
@@ -8,39 +7,44 @@ import { Loader } from "./";
 
 import { TransactionContext } from "../context/TransactionContext";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
+
 export default function Welcome() {
-  const handleSubmit = () => {
+  const { connectWallet, currentAccount, sendTransaction } =
+    useContext(TransactionContext);
+
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: "",
+  });
+
+  const handleChange = (e, name) => {
+    setFormData({ ...formData, [name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
+
     e.preventDefault();
 
-    if (!addressTo || !amount || !keyword || !message) {
-      return alert("Please fill all the fields");
-    }
+    if (!addressTo || !amount || !keyword || !message) return;
+
     sendTransaction();
   };
 
-  const Input = ({ placeholder, name, type, value, handleChange }) => (
-    <input
-      placeholder={placeholder}
-      type={type}
-      step="0.0001"
-      value={value}
-      onChange={(e) => handleChange(e, name)}
-      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-    />
-  );
-
   const commonStyles =
     "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-
-  const {
-    connectWallet,
-    currentAccount,
-    formData,
-    setFormData,
-    handleChange,
-    sendTransaction,
-  } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -65,12 +69,12 @@ export default function Welcome() {
           )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl} ${commonStyles}`}>Reliability</div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
             <div className={commonStyles}>Security</div>
-            <div className={`rounded-tr-2xl} ${commonStyles}`}>Etherium</div>
-            <div className={`rounded-bl-2xl} ${commonStyles}`}>Web 3.0</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
             <div className={commonStyles}>Low Fees</div>
-            <div className={`rounded-br-2xl} ${commonStyles}`}>Blockchain</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
 
@@ -81,7 +85,7 @@ export default function Welcome() {
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
                   <SiEthereum fontSize={21} color="#fff" />
                 </div>
-                <BsInfoCircle fontSize={17} color="#fff"></BsInfoCircle>
+                <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
                 <p className="text-white font-light text-sm">Address</p>
@@ -97,24 +101,28 @@ export default function Welcome() {
               placeholder="Address To"
               name="addressTo"
               type="text"
+              value={formData.addressTo}
               handleChange={handleChange}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
+              value={formData.amount}
               handleChange={handleChange}
             />
             <Input
               placeholder="Keyword (GIF)"
               name="keyword"
               type="text"
+              value={formData.keyword}
               handleChange={handleChange}
             />
             <Input
               placeholder="Enter your message"
               name="message"
               type="text"
+              value={formData.message}
               handleChange={handleChange}
             />
             <div className="h-[1px] w-full bg-gray-400 my-2"></div>
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -10,25 +10,35 @@ const { ethereum } = window;
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
-  const transactionContract = new ethers.Contract(
+  const transactionsContract = new ethers.Contract(
     contractAddress,
     contractABI,
     signer
   );
 
-  console.log({ provider, signer, transactionContract });
+  return transactionsContract;
 };
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
-  const [formData, setFormData] = useState({addressTo: "", amount: "", keyword: "", message: ""});
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [transactionCount, setTransactionCount] = useState(
+    localStorage.getItem("transactionCount") || 0
+  );
 
   const handleChange = (e, name) => {
     setFormData((prevState) => ({
-        ...prevState,
-        [name]: e.target.value,
+      ...prevState,
+      [name]: e.target.value,
     }));
-  }
+  };
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -65,15 +75,50 @@ export const TransactionProvider = ({ children }) => {
     }
   };
 
-  const sendTransaction = async (amount) => {
+  const sendTransaction = async () => {
     try {
-        if (!ethereum) {
-            return alert("Please install MetaMask!");
-        }
+      if (ethereum) {
+        const { addressTo, amount, keyword, message } = formData;
+        const transactionsContract = getEthereumContract();
+        const parsedAmount = ethers.utils.parseEther(amount);
 
+        await ethereum.request({
+          method: "eth_sendTransaction",
+          params: [
+            {
+              from: currentAccount,
+              to: addressTo,
+              gas: "0x5208",
+              value: parsedAmount._hex,
+            },
+          ],
+        });
+
+        const transactionHash = await transactionsContract.addToBlockchain(
+          addressTo,
+          parsedAmount,
+          message,
+          keyword
+        );
+
+        setIsLoading(true);
+        console.log(`Loading - ${transactionHash.hash}`);
+        await transactionHash.wait();
+        console.log(`Success - ${transactionHash.hash}`);
+        setIsLoading(false);
+
+        const transactionsCount =
+          await transactionsContract.getTransactionCount();
+
+        setTransactionCount(transactionsCount.toNumber());
+        window.location.reload();
+      } else {
+        console.log("No ethereum object");
+      }
     } catch (error) {
-        console.error(error);
-        throw new Error("Error sending transaction");
+      console.log(error);
+
+      throw new Error("No ethereum object");
     }
   };
 
@@ -82,8 +127,16 @@ export const TransactionProvider = ({ children }) => {
   }, []);
 
   return (
-    
-    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction }}>
+    <TransactionContext.Provider
+      value={{
+        connectWallet,
+        currentAccount,
+        formData,
+        setFormData,
+        handleChange,
+        sendTransaction,
+      }}
+    >
       {children}
     </TransactionContext.Provider>
   );
